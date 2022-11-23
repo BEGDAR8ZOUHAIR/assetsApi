a27ab83d2d4c11bb1c2b20c6c50d52c4b89f32b7
BEGDAR8ZOUHAIR <begdar8zouhair@gmail.com>
2022-11-23 16:30:47 +0100

    add structure MVC

---
+++ server/api/server.js
@@ -0,0 +1,18 @@
+const express = require("express"); // import express
+const dotenv = require("dotenv").config(); // import dotenv
+const colors = require("colors"); // import colors
+const { errorHandler } = require("./middleware/errorMiddleware"); // custom error handler
+const port = process.env.PORT || 6000; // set our port
+const app = express();
+const connectDB = require("./config/db"); // import db connection
+
+connectDB(); // connect to db
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.use('/admin', require('./routes/adminRoutes'));
+
+
+app.use(errorHandler)
+app.listen(port, () => console.log(`Server started on port ${port}`));
