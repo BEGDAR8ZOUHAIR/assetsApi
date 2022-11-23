8c85d0d6045ba744c37a349edf0c003e42f9248c
BEGDAR8ZOUHAIR <begdar8zouhair@gmail.com>
2022-11-23 21:48:05 +0100

    add auth middelwere

--- server/api/routes/goalRoutes.js
+++ server/api/routes/goalRoutes.js
@@ -12,4 +12,5 @@ const { protect } = require('../middleware/authMiddleware')
 router.route('/').get(protect, getGoals).post(protect, setGoal)
 router.route('/:id').delete(protect, deleteGoal).put(protect, updateGoal)
 
+
 module.exports = router
