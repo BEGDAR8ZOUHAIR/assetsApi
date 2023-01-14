f8878f83eb52b888e6dd0ca201fdf66552363c4b
BEGDAR8ZOUHAIR <begdar8zouhair@gmail.com>
2023-01-14 01:00:16 +0100

    add transaction list

--- expense-tracker/src/App.js
+++ expense-tracker/src/App.js
@@ -2,6 +2,7 @@
 import Header from "./components/Header"
 import Balance from "./components/Balance";
 import IncomeExxpenses from "./components/IncomeExxpenses";
+import TransactionList from "./components/TransactionList";
 
 import './index.css';
 function App()
@@ -12,7 +13,7 @@ function App()
       <div className="container">
         <Balance />
         <IncomeExxpenses />
-
+        <TransactionList />
       </div>
 
     </>
