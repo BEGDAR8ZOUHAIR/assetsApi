bc93e55d3e2fa86b25d9f72ff504a7cc35fcc72b
BEGDAR8ZOUHAIR <begdar8zouhair@gmail.com>
2023-01-14 02:21:01 +0100

    delete transaction

--- expense-tracker/src/components/Transaction.jsx
+++ expense-tracker/src/components/Transaction.jsx
@@ -1,13 +1,20 @@
-import React from 'react'
+import React, { useContext } from "react";
+import { GlobalContext } from "../context/GlobalState";
 
 export default function Transaction({ transaction })
 {
-    const sign = transaction.amount < 0 ? '-' : '+';
+    const { deleteTransaction } = useContext(GlobalContext);
+  const sign = transaction.amount < 0 ? "-" : "+";
   return (
-      <li className={transaction.amount < 0 ? 'minus' : 'plus'}>
-          {/* Math.abs() returns the absolute value of a number */}
-          {transaction.text} <span>{sign}${Math.abs(transaction.amount)}</span> 
-      <button className="delete-btn">x</button>
+    <li className={transaction.amount < 0 ? "minus" : "plus"}>
+      {/* Math.abs() returns the absolute value of a number */}
+      {transaction.text}{" "}
+      <span>
+        {sign}${Math.abs(transaction.amount)}
+      </span>
+          <button
+        onClick={() => deleteTransaction(transaction.id)}
+              className="delete-btn">x</button>
     </li>
   );
 }
