c2a275e304f3d0a2b535befe6ff2551b1cb36fae
BEGDAR8ZOUHAIR <begdar8zouhair@gmail.com>
2023-01-14 02:11:04 +0100

    add income and expence

--- expense-tracker/src/components/IncomeExxpenses.jsx
+++ expense-tracker/src/components/IncomeExxpenses.jsx
@@ -1,22 +1,28 @@
-import React from 'react'
 
-export default function IncomeExxpenses() {
+import React, { useContext } from 'react'
+import { GlobalContext } from '../context/GlobalState'
+
+export default function IncomeExpenses()
+{
+  const { transactions } = useContext(GlobalContext);
+  const amounts = transactions.map(transaction => transaction.amount);
+  const income = amounts
+    .filter(item => item > 0)
+    .reduce((acc, item) => (acc += item), 0)
+    .toFixed(2);
+  const expense = (
+    amounts.filter(item => item < 0).reduce((acc, item) => (acc += item), 0) * -1
+  ).toFixed(2);
   return (
-    <>
-      <div className="inc-exp-container">
-        <div>
-          <h4>Income</h4>
-          <p  className="money plus">
-            +$0.00
-          </p>
-        </div>
-        <div>
-          <h4>Expense</h4>
-          <p className="money minus">
-            -$0.00
-          </p>
-        </div>
+    <div className="inc-exp-container">
+      <div>
+        <h4>Income</h4>
+        <p className="money plus">${income}</p>
       </div>
-    </>
+      <div>
+        <h4>Expense</h4>
+        <p className="money minus">${expense}</p>
+      </div>
+    </div>
   );
 }
