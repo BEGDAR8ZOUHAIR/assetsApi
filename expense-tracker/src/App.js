ff7180f0920ab1141be02069f36c3549327fe789
BEGDAR8ZOUHAIR <begdar8zouhair@gmail.com>
2023-01-14 01:04:08 +0100

    add traansaction to add transaction

--- expense-tracker/src/App.js
+++ expense-tracker/src/App.js
@@ -3,6 +3,7 @@ import Header from "./components/Header"
 import Balance from "./components/Balance";
 import IncomeExxpenses from "./components/IncomeExxpenses";
 import TransactionList from "./components/TransactionList";
+import AddTransaction from "./components/AddTransaction";
 
 import './index.css';
 function App()
@@ -14,6 +15,7 @@ function App()
         <Balance />
         <IncomeExxpenses />
         <TransactionList />
+        <AddTransaction />
       </div>
 
     </>
